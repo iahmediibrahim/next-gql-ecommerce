ba0697a38d05942065b208b356bd4d2d71ba0d39
Ahmed Ibrahim <iahmediibrahim@gmail.com>
2022-04-10 19:37:09 +0200

    url updated

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,9 @@
 import { GraphQLClient } from 'graphql-request'
 import { QueryClient } from 'react-query'
 import { getSdk } from './generated/graphql'
+// updated
 const gqlClient = new GraphQLClient('https://next-gql-ecommerce.vercel.app/api/graphql')
+
 export const { getProducts, productByName } = getSdk(gqlClient)
 export const queryClient = new QueryClient({
 	defaultOptions: {
